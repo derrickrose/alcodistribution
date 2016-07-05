910953fb343ffdb83cb66b97755f7814a43b5eb4
Rose <Rose@BOSS>
2016-07-05 06:17:33 +0300

    fix on ihm.FormHandler

--- src/com/pushtech/ihm/FormHandler.java
+++ src/com/pushtech/ihm/FormHandler.java
@@ -3,8 +3,10 @@ package com.pushtech.ihm;
 import org.apache.commons.lang3.StringUtils;
 
 public class FormHandler {
-	
-	private static Form pushTechConsultingForm = null;//this long name in order to avoid wrong access memory 
+
+	private static Form pushTechConsultingForm = null;// this long name in order
+														// to avoid wrong access
+														// memory
 
 	public static void validateForm(String linkToCrawl, String dataBaseLink,
 			String dataBaseUser, String userPassWord) {
@@ -12,9 +14,18 @@ public class FormHandler {
 		dataBaseLink = validateField(dataBaseLink);
 		dataBaseUser = validateField(dataBaseUser);
 		userPassWord = validateField(userPassWord);
-		pushTechConsultingForm = new Form(linkToCrawl, dataBaseLink, dataBaseUser, userPassWord);
+		pushTechConsultingForm = new Form(linkToCrawl, dataBaseLink,
+				dataBaseUser, userPassWord);
 	}
 
+	public static Form getForm(String linkToCrawl, String dataBaseLink,
+			String dataBaseUser, String userPassWord) {
+		if (pushTechConsultingForm == null) {
+			validateForm(linkToCrawl, dataBaseLink, dataBaseUser, userPassWord);
+		}
+		return pushTechConsultingForm;
+	}
+	
 	public static Form getForm() {
 		return pushTechConsultingForm;
 	}
