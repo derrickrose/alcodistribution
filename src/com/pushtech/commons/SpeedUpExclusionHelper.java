037a84b5b2c2f5379d969a75d2f6e614a368a36a
srazafindrazaka <srazafindrazaka@valueit.fr>
2016-07-08 21:02:38 +0300

    fix speedup

--- src/com/pushtech/commons/SpeedUpExclusionHelper.java
+++ src/com/pushtech/commons/SpeedUpExclusionHelper.java
@@ -11,6 +11,8 @@ public class SpeedUpExclusionHelper {
       initList();
    }
 
+   private ArrayList<String> include = new ArrayList<String>();
+
    // private void initList() {
    // exclude.add("CADEAU");
    // exclude.add("EXPOSANTS");
@@ -43,6 +45,14 @@ public class SpeedUpExclusionHelper {
       // exclude.add("PROMOTION");
       // exclude.add("SUPER%20PROMO");
       // exclude.add("UEFA%20EURO");
+   }
+
+   public ArrayList<String> getInclude() {
+      return include;
+   }
+
+   public void setInclude(ArrayList<String> include) {
+      this.include = include;
    }
 
    public ArrayList<String> getExclude() {
